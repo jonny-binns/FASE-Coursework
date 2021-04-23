325bc122eca087fd8b7435457d22d47bde9c7630
Jonathan Binns <jonny.binns1@gmail.com>
2021-04-23 17:36:14 +0100

    Added: Plane cant take off without doors being shut, Plane cant take off if theres not enough fuel on board, low fuel warning, airspeed/altitude maintained

--- src/aeroplane.adb
+++ src/aeroplane.adb
@@ -1,3 +1,105 @@
-Package body Aeroplane with SPARK_Mode
+with Ada.Text_IO; use Ada.Text_IO;
+
+
+Package body Aeroplane with SPARK_Mode is
+
+--methods
+
+--shut door
+   procedure ShutDoor (d : in out door) is
+   begin
+      d.Status := closed;
+      Put_Line("Door is: " & d.Status'Image);
+   end ShutDoor;
+
+--open door
+  procedure OpenDoor (d : in out Door) is
+  begin
+      d.Status := open;
+      Put_Line("Door is: " & d.Status'Image);
+  end OpenDoor;
+
+--lock door
+  procedure LockDoor (d : in out Door) is
+  begin
+      d.Lock := locked;
+      Put_Line("Door is: " & d.Lock'Image);
+   end LockDoor;
+
+--unlock door
+  procedure UnlockDoor (d : in out Door) is
+  begin
+      d.Lock := unlocked;
+      Put_Line("Door is: " & d.Lock'Image);
+ end UnlockDoor;
+
+
+   --set fuel
+   procedure SetFuel (f : out FuelLevel; i : in FuelLevel) is
+   begin
+      f := i;
+      Put_Line("Fuel is: " & f'Image & "% full");
+   end SetFuel;
+
+
+--take off
+   procedure TakeOff ( p : in out Aeroplane) is
+   begin
+      p.Flight := takeOff;
+      Put_Line("Flight set to" & p.Flight'Image);
+   end TakeOff;
+
+
+   --maintain normal flight
+   procedure MaintainNormalFlight (p : in out Aeroplane) is
+   begin
+      --altitude
+      while p.Altitude < 35000 loop
+         p.Altitude := p.Altitude + 1;
+         if p.Altitude mod 100 = 0 then
+            Put_Line("Altitude: " & p.Altitude'Image & " ft");
+         end if;
+      end loop;
+      while p.Altitude > 35000 loop
+         p.Altitude := p.Altitude - 1;
+         if p.Altitude mod 100 = 0 then
+            Put_Line("Altitude: " & p.Altitude'Image & " ft");
+         end if;
+      end loop;
+
+      --airspeed
+      while p.AirSpeed < 933 loop
+         p.AirSpeed := p.AirSpeed + 1;
+         if p.Altitude mod 5 = 0 then
+            Put_Line("Air Speed: " & p.AirSpeed'Image & " km/h");
+         end if;
+      end Loop;
+      while p.AirSpeed > 933 loop
+         p.AirSpeed := p.AirSpeed - 1;
+         if p.Altitude mod 5 = 0 then
+            Put_Line("Air Speed: " & p.AirSpeed'Image & " km/h");
+         end if;
+      end Loop;
+
+      Put_Line("Altitude: " & p.Altitude'Image & " ft");
+      Put_Line("Air Speed: " & p.AirSpeed'Image & " km/h");
+   end MaintainNormalFlight;
+
+
+   --flight status
+   procedure FlightStatus (p : in Aeroplane) is
+   begin
+      Put_Line("Cockpit door is: " & p.CockpitDoor.Status'Image & " and " & p.CockpitDoor.Lock'Image);
+      Put_Line("External doors are: " & p.ExternalDoor.Status'Image & " and " & p.ExternalDoor.Lock'Image);
+      Put_Line("Flight stage: " & p.Flight'Image);
+      Put_Line("Altitude: " & p.Altitude'Image & " ft");
+      Put_Line("Air Speed: " & p.AirSpeed'Image & " km/h");
+      if p.Fuel <= 20 then
+         Put_Line("-------------------- WARNING FUEL LEVEL LOW --------------------");
+      end if;
+      Put_Line("Fuel: " & p.Fuel'Image & "%");
+
+   end FlightStatus;
+
 
 end Aeroplane;
--- src/aeroplane.ads
+++ src/aeroplane.ads
@@ -1,3 +1,102 @@
-package Aeroplane with SPARK_mode
+package Aeroplane with SPARK_mode is
+
+  --Variables
+  --door, status (open/closed), lock(locked, unlocked)
+   type DStatus is (open, closed);
+   type DLock is (unlocked, locked);
+
+   type Door is record
+      Status : Dstatus;
+      Lock : DLock;
+   end record;
+
+  --flight status, (stationary, takeoff, normal, landing, tow)
+   type FlightStage is (stationary, takeOff, normal, landing, tow);
+
+   --fuel level (0..100), modelled as %
+   type FuelLevel is range 0..100;
+
+   --AirSpeed in km/h
+   type AirSpeedRange is range 0..1000;
+
+   --Altitude in ft
+   type AltitudeRange is range 0..50000;
+
+   --contains all variables needed for an aeroplane
+   --variables are set to status needed for take off by default
+  type Aeroplane is record
+      CockpitDoor : Door := (Status => open, Lock => unlocked);
+      ExternalDoor : Door := (Status => open, Lock => unlocked);
+      Flight : FlightStage := stationary;
+      Fuel : FuelLevel := 100;
+      --is the minimum amount of fuel needed for takeoff
+      MinimumFuel : FuelLevel := 50;
+      Altitude : AltitudeRange := 0;
+      AirSpeed : AirSpeedRange := 0;
+   end record;
+
+
+--Methods
+
+--shut door
+--takes door as input and shuts it
+  procedure ShutDoor (d : in out Door) with
+     Pre => d.Status = open,
+     Post => d.Status = closed;
+
+--open door
+--takes door as input and opens it
+  procedure OpenDoor (d : in out Door) with
+     Pre => d.Status = closed,
+     Post => d.Status = open;
+
+--lock door
+--takes door and locks it
+   procedure LockDoor (d : in out Door) with
+     Pre => d.Lock = unlocked,
+     Post => d.Lock = locked;
+
+--unlock door
+--takes door and unlocks it
+   procedure UnlockDoor (d : in out Door) with
+    Pre => d.Lock = locked,
+     Post => d.Lock = unlocked;
+
+
+   --set fuel
+   --allows fuel to be set to a specific level
+   procedure SetFuel(f : out FuelLevel; i : in FuelLevel) with
+     Pre => i <= FuelLevel'Last,
+   Post => f <=FuelLevel'Last;
+
+ --decrease fuel to level
+
+
+--take off
+--checks all doors are closed and locked
+--takes amount of fuel needed and checks that amount is onboard
+--sets flight status to takeoff
+   procedure TakeOff (p : in out Aeroplane) with
+     Pre => p.CockpitDoor.Status = closed and p.CockpitDoor.Lock = locked
+     and p.ExternalDoor.Status = closed and p.ExternalDoor.Lock = locked
+     and p.Fuel >= p.MinimumFuel,
+     Post => p.Flight = takeOff;
+
+
+   --maintain normal flight
+   --keeps the plane within altitude and airspeed requirements
+   -- values for post conditions were taken from googling average/max cruising speed and altitude for boing 737
+   -- pre, within range
+   -- post, altitude should be = 35000, speed should be = 933
+   procedure MaintainNormalFlight (p : in out Aeroplane) with
+     Post => p.Altitude = 35000 and p.AirSpeed = 933;
+
+
+   --Flight status
+   -- returns the status of the plane and warns if fuel is low
+   procedure FlightStatus (p : in  Aeroplane);
+
+
+
 
 end Aeroplane;
